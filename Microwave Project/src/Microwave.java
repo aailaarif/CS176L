3959647f1a5e984907002f275b9ecb668d26020a
aailaarif <113384742+aailaarif@users.noreply.github.com>
2022-09-28 20:14:17 -0400

    My Microwave Project

---
+++ Microwave Project/src/Microwave.java
@@ -0,0 +1,41 @@
+
+public class Microwave {
+	
+	//instantiate instance variables with default values
+	private int power = 1;
+	private int time = 0;
+	//output time and power level the Microwave object is cooking for
+	public void start () {
+		
+		System.out.println("Cooking for " + time + " seconds at level " + power + ".");
+	}
+	//setter method to add 30 seconds to current time and output
+	public void time () {
+		
+		time = time + 30;
+		System.out.println("Time button was pressed. Time is " + time + " seconds");
+	}
+	//switch between power levels 1 and 2 unless power is equal to 0 and output
+	public void power () {
+		if(power==0) {
+			power++;
+			System.out.println("Power button was pressed. Power level is " + power);
+		}
+		else if (power==1) {
+			power++;
+			System.out.println("Power button was pressed. Power level is " + power);
+		}
+		else {
+			power=1;
+			System.out.println("Power button was pressed. Power level is " + power);
+		}
+	}
+	//reset power and time attributes to default and output
+	public void reset () {
+		
+		power = 1;
+		time = 0;
+		System.out.println("Reset button was pressed. Power level is at " + power + ". Time is " + time + " seconds.");
+	}
+
+}
---
+++ Microwave Project/src/MicrowaveTester.java
@@ -0,0 +1,26 @@
+
+public class MicrowaveTester {
+	
+	public static void main(String args[]) {
+		//instantiate a new object of the Microwave class called myMicrowave
+		Microwave myMicrowave = new Microwave();
+		//call the method time to add 30 seconds to the time twice and output
+		myMicrowave.time();
+		myMicrowave.time();
+		//call power method once to switch the power level and output
+		myMicrowave.power();
+		//call start method to output current time and power level
+		myMicrowave.start();
+		//reset time and power attributes to default with reset method call
+		myMicrowave.reset();
+	}
+
+}
+
+/* Output
+Time button was pressed. Time is 30 seconds
+Time button was pressed. Time is 60 seconds
+Power button was pressed. Power level is 2
+Cooking for 60 seconds at level 2.
+Reset button was pressed. Power level is at 1. Time is 0 seconds.
+*/
